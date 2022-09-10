a91b580d893267711e1cf4f7a0974a0e3ae5dae8
Bruno Gonçalves <brgoncalves@hotmail.com.br>
2022-09-10 15:33:12 -0300

    logo Header to home

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,14 +5,20 @@ import logoImg from "../assets/logo.svg"
 import { Container, Header } from "../styles/pages/app"
 
 import Image from "next/future/image"
+import Link from "next/link"
 
 globalStyles()
+const cancelUrl = `${process.env.NEXT_URL}/`;
 
 function App({ Component, pageProps }: AppProps) {
   return (
+   
+
     <Container>
       <Header>
+      <Link href="/" >
       <Image src={logoImg} alt="" />
+      </Link>
       </Header>
 
       <Component {...pageProps} />
